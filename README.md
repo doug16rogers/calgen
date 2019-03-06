95a9a6c46ce96d084e4eb2220901757061f6a1d2
Your Name <you@example.com>
2019-03-05 21:06:23 -0500

    Finish off Loudoun United FC's 2019 calendar.

--- README.md
+++ README.md
@@ -1,13 +1,100 @@
 # Converting calendars from online
 
+## Standard for calendars
+
+The iCalendar standard (RFC-5545, .ics files) discusses the VEVENT field at
+https://tools.ietf.org/html/rfc5545#section-3.6.1. Below is a sample calendar
+from section 3.4 of the RFC:
+
+```
+    BEGIN:VCALENDAR
+    VERSION:2.0
+    PRODID:-//hacksw/handcal//NONSGML v1.0//EN
+    BEGIN:VEVENT
+    UID:19970610T172345Z-AF23B2@example.com
+    DTSTAMP:19970610T172345Z
+    DTSTART:19970714T170000Z
+    DTEND:19970715T040000Z
+    SUMMARY:Bastille Day Party
+    END:VEVENT
+    END:VCALENDAR
+```
+
+Converting to UTC might be a pain in these scripts. Eh, 'date' does the trick
+but it requires extensions that differ across unices.
+
 ## DC United
 
-They do not provide an ical - they funnel everything through ecal.com, which
-wants to have access to your calendar.
+They do not provide a calendar in a standard format - they funnel everything
+through ecal.com, which wants to have access to your calendar.
 
 Use mls2cal.sh and pass the URL of the team's MLS schedule page. It defaults
 to DC United's page. It will print the commands it is running so you can see
 how it works if you care.
+
+## Loudoun United FC
+
+The scheulde web page is https://www.loudoununitedfc.com/schedule-2019.
+
+See lufc2cal.sh for details.
+
+Here's an excerpt of the results of the final run:
+
+```
+    BEGIN:VCALENDAR
+    VERSION:2.0
+    PRODID:-//drcal/handcal//NONSGML v0.1//EN
+    BEGIN:VEVENT
+    UID:20190310T000000Z-1@loudoununitedfc.com
+    DTSTAMP:20190310T010000Z
+    DTSTART:20190310T010000Z
+    DTEND:20190310T030000Z
+    SUMMARY:[Away] Loudoun United FC @ Nashville SC
+    LOCATION:First Tennessee Park
+    END:VEVENT
+    BEGIN:VEVENT
+    UID:20190317T000000Z-1@loudoununitedfc.com
+    DTSTAMP:20190317T000000Z
+    DTSTART:20190317T000000Z
+    DTEND:20190317T020000Z
+    SUMMARY:[Away] Loudoun United FC @ Memphis 901 FC
+    LOCATION:AutoZone Park
+    END:VEVENT
+    ...
+    END:VCALENDAR
+```
+
+This was tested on both Google Calendar and Apple's MacOS Calendar.
+
+## Deleting the calendar events
+
+To have the events be removed, add STATUS:CANCELLED to each event, like:
+
+```
+    BEGIN:VCALENDAR
+    VERSION:2.0
+    PRODID:-//drcal/handcal//NONSGML v0.1//EN
+    BEGIN:VEVENT
+    UID:20190310T000000Z-1@loudoununitedfc.com
+    STATUS:CANCELLED
+    DTSTAMP:20190310T010000Z
+    DTSTART:20190310T010000Z
+    DTEND:20190310T030000Z
+    SUMMARY:[Away] Loudoun United FC @ Nashville SC
+    LOCATION:First Tennessee Park
+    END:VEVENT
+    BEGIN:VEVENT
+    UID:20190317T000000Z-1@loudoununitedfc.com
+    STATUS:CANCELLED
+    DTSTAMP:20190317T000000Z
+    DTSTART:20190317T000000Z
+    DTEND:20190317T020000Z
+    SUMMARY:[Away] Loudoun United FC @ Memphis 901 FC
+    LOCATION:AutoZone Park
+    END:VEVENT
+    ...
+    END:VCALENDAR
+```
 
 
 
--- lufc2cal.sh
+++ lufc2cal.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-site=https://www.loudoununitedfc.com/schedule-2019
+version=1
+year=2019
+site=https://www.loudoununitedfc.com/schedule-$year
 
-if [ -n "$1" ]; then
+if [[ $(uname -s) != Darwin ]]; then
+    echo "$0: only supported on Darwin (MacOS); fixme by changing the 'date' code." 1>&2
+    exit 1
+fi
+
+if [[ -n "$1" ]]; then
     site="$1"
 fi
 
@@ -10,16 +17,164 @@ tmpsched=tmpsched.html
 tmpfiltered=tmpfiltered.txt
 tmpfields=tmpfields.txt
 
-echo curl -s -o $tmpsched "$site"
+#if [ ! -t ]; then     # REMOVE ME!!!
+
+echo curl -s -o $tmpsched "$site" 1>&2
 curl -s -o $tmpsched "$site"
-if [ $? -ne 0 ]; then echo curl failed; exit 1; fi
+if [ $? -ne 0 ]; then echo curl failed 1>&2; exit 1; fi
 
-echo egrep '(<h6 style="text|^<h1 style="text|span style="color:#|</span></span></p>)' $tmpsched
+echo egrep '(<h6 style="text|^<h1 style="text|span style="color:#|</span></span></p>)' $tmpsched 1>&2
 egrep '(<h6 style="text|^<h1 style="text|span style="color:#|</span></span></p>)' $tmpsched > $tmpfiltered
-if [ $? -ne 0 ]; then echo egrep failed; exit 1; fi
+if [ $? -ne 0 ]; then echo egrep failed 1>&2; exit 1; fi
 
-echo "cat $tmpfiltered | sed -e 's/<span style=.color:#FFFFFF.>/month=/' | sed -e 's/<[^>]*>//g'"
-cat $tmpfiltered | sed -e 's/<span style=.color:#FFFFFF.>/month=/' | sed -e 's/<[^>]*>//g' > $tmpfields
-if [ $? -ne 0 ]; then echo sed failed; exit 1; fi
-head -8 $tmpfields
+echo "cat $tmpfiltered \
+    | sed -e 's/<span style=.color:#FFFFFF.>/month=/' \
+ -e 's/<[^>]*>//g' -e 's/&nbsp;/ /g' \
+ -e 's/<[^>]*>//g' -e \"s/&rsquo;/'/g\"" 1>&2
+cat $tmpfiltered \
+    | sed -e 's/<span style=.color:#FFFFFF.>/month=/' \
+          -e 's/<[^>]*>//g' \
+          -e 's/&nbsp;/ /g' \
+          -e "s/&rsquo;/'/g" \
+    | sed -e 's/<[^>]*>//g' \
+    > $tmpfields
+if [ $? -ne 0 ]; then echo sed failed 1>&2; exit 1; fi
 
+#fi     # REMOVE ME!!!
+
+#**
+#* Echo the month number for the month name in $1.
+#*
+month_number() {
+    local lo=$(echo $1 | tr '[:upper:]' '[:lower:]')
+    case $lo in
+        jan*) echo 1 ;;
+        feb*) echo 2 ;;
+        mar*) echo 3 ;;
+        apr*) echo 4 ;;
+        may*) echo 5 ;;
+        jun*) echo 6 ;;
+        jul*) echo 7 ;;
+        aug*) echo 8 ;;
+        sep*) echo 9 ;;
+        oct*) echo 10 ;;
+        nov*) echo 11 ;;
+        dec*) echo 12 ;;
+        *) echo 0
+    esac
+}   # month_number()
+
+is_lo_weekday() {
+    case $1 in
+        mon*) return 0 ;;
+        tue*) return 0 ;;
+        wed*) return 0 ;;
+        thu*) return 0 ;;
+        fri*) return 0 ;;
+        sat*) return 0 ;;
+        sun*) return 0 ;;
+        *) return 1 ;;
+    esac
+}
+
+is_lo_home_away() {
+    case $1 in
+        home) return 0 ;;
+        away) return 0 ;;
+        *) return 1 ;;
+    esac
+}
+
+is_day() {
+    numeric=$(echo $1 | sed -e 's/[^0-9]//g')
+    if [ "$numeric" == "$1" ]; then
+        return 0
+    fi
+    return 1
+}
+
+is_opponent() {
+    case $1 in
+        @*) return 0 ;;
+        vs.*) return 0 ;;
+        *) return 1 ;;
+    esac
+}
+
+# Eh. I could write a state machine. But this is quick and dirty.
+weekday_name=Saturday
+month_name=February
+month=2
+day=1
+opponent="Who Are Ya FC"
+lo_home_away=home
+home_away=home
+
+echo BEGIN:VCALENDAR
+echo VERSION:2.0
+echo PRODID:-//drcal/handcal//NONSGML v0.1//EN
+
+cat $tmpfields | while read line; do
+    line=$(echo $line | tr -d '\r\n')
+    whiteless=$(echo $line | tr -d '[:space:]')
+    lo=$(echo $whiteless | tr '[:upper:]' '[:lower:]')
+    if [[ "${whiteless#*month=}" != "$whiteless" ]]; then
+        month_name=$(echo $whiteless | sed -e 's/.*month=\([A-Z][a-z]*\).*/\1/')
+        month=$(month_number $month_name)
+        #echo "New month: $month_name ($month)"
+    elif is_lo_weekday $lo; then
+        weekday_name=$whiteless
+        #echo "New weekday: $weekday_name"
+    elif is_lo_home_away $lo; then
+        lo_home_away=$lo
+        home_away=$whiteless
+        #echo "New home/away: $home_away"
+    elif is_day $lo; then
+        day=$lo
+        #echo "New day: $day"
+    elif is_opponent $lo; then
+        opponent=$(echo $line | sed -e 's/^\(@ \|vs. \)//')
+        #echo "New opponent: $opponent"
+    else
+        time=$(echo $line | cut '-d ' -f1)
+        zone=$(echo $line | cut '-d ' -f2)
+        venue=$(echo $line | cut '-d ' -f4-)
+        if [[ "$venue" == "TBD" ]]; then
+            venue="Venue TBD"
+        fi
+        if [[ "$time" == "Time" ]]; then
+            time="8:00am"
+            opponent="$opponent [Time TBD]"
+        fi
+        hour=$(echo $time | sed -e 's/^\([0-9]*\).*/\1/')
+        minute=$(echo $time | sed -e 's/^[0-9]*:\([0-9]*\).*/\1/')
+        second=0
+        if [[ "${time#*pm}" != "$time" ]]; then
+            hour=$[hour + 12]
+        fi
+
+        #printf "New event: date=%04d%02d%02d time=%02d%02d%02d zone=%s venue='%s'\n" \
+        #    $year $month $day $hour $minute $second "$zone" "$venue"
+
+        # Darwin-date-specific:
+        timespec=$(printf %02d%02d%02d%02d%04d $month $day $hour $minute $year)
+        epoch_seconds=$(date -j $timespec +%s)
+        # Assume EDT:
+        utc_start=$(date -j -r $[epoch_seconds + (4 * 3600)] +%Y%m%dT%H%M%SZ)
+        utc_end=$(date -j -r $[epoch_seconds + (6 * 3600)] +%Y%m%dT%H%M%SZ)
+        #printf "           start=%s end=%s venue='%s'\n" $utc_start $utc_end "$venue"
+
+        uid=$utc_start-$version@loudoununitedfc.com
+
+        echo BEGIN:VEVENT
+        echo UID:$uid
+        echo DTSTAMP:$utc_start
+        echo DTSTART:$utc_start
+        echo DTEND:$utc_end
+        echo SUMMARY:[$home_away] Loudoun United FC $opponent
+        echo LOCATION:$venue
+        echo END:VEVENT
+    fi
+done
+
+echo END:VCALENDAR
